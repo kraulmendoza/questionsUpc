96bd2cbd1419e7661c302f097a3dfc63542123ba
kraulmendoza <knraulmendoza@gmail.com>
2019-07-27 11:55:39 -0500

    updatePreguntas

--- src/app/pages/preguntas/preguntas.page.ts
+++ src/app/pages/preguntas/preguntas.page.ts
@@ -9,29 +9,49 @@ import { GlobalService } from 'src/app/services/global.service';
   styleUrls: ['./preguntas.page.scss'],
 })
 export class PreguntasPage implements OnInit {
-  preguntas: iPregunta[] = [{
-    descripcion: '¿cuando es 2 +2?',
-    opciones: ['1','2','3','4'],
-    respuesta: 2
-  }];
+  preguntas: iPregunta[] = [
+    {
+      descripcion: '¿cuando es 2 +2?',
+      opciones: ['1','2','3','4'],
+      respuesta: 2
+    },
+    {
+      descripcion: '¿cuando es 6 +2?',
+      opciones: ['5','2','3','8'],
+      respuesta: 4
+    }
+  ];
+  indexPregunta = 0;
   btnContinuar = true;
   duracion = 15;
   colorDuracion = '';
+  respuesta = 0;
+  intervalo;
+  stateOpc = false;
+  state = false;
   constructor(private actionSheet: ActionSheetController, private global: GlobalService) { }
 
   ngOnInit() {
-    const intervalo = setInterval(() => {
+    this.empezar();
+  }
+
+  empezar(){
+    this.intervalo = setInterval(() => {
       this.duracion--;
       if (this.duracion < 10) {
         this.colorDuracion = 'danger';
       }
       if (this.duracion == 0) {
         this.global.mensaje('se acabo el tiempo', 3000, 'danger');
-        clearInterval(intervalo);
+        clearInterval(this.intervalo);
       }
     }, 1000);
   }
 
+  desordenar(array){
+    var t = array.sort(function(a,b) {return (Math.random()-0.5)});
+    return [...t];
+  }
   async openHelp(){
     const action = await this.actionSheet.create({
       header: 'Opciones de ayuda',
@@ -50,6 +70,25 @@ export class PreguntasPage implements OnInit {
       ]
     });
     return await action.present();
+  }
+  show(res) {
+    this.respuesta = parseInt(res);
+  }
+
+  enviar(){
+    if (this.respuesta === this.preguntas[this.indexPregunta].respuesta) {
+      this.global.mensaje('correcto', 2000, 'success');
+      this.btnContinuar = false;
+      clearInterval(this.intervalo);
+    }else{
+      this.global.mensaje('Respuesta incorrecta', 2000, 'danger');
+    }
+  }
+  continuar(){
+    this.indexPregunta++;
+    this.duracion = 15;
+    this.colorDuracion = '';
+    this.empezar();
   }
 
 }
